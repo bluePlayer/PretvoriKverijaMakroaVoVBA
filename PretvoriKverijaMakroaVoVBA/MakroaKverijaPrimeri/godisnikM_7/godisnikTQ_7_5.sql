6b011d3799e3ceff7e47c3f370ba7ba877c594b8
bluePlayer <vlzakar@gmail.com>
2025-12-25 19:06:35 +0100

    Ги избришав кверијата за годишник-7.

--- PretvoriKverijaMakroaVoVBA/MakroaKverijaPrimeri/godisnikM_7/godisnikTQ_7_5.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO Tgodisnik_7 ( GOD, sort, NAZIV )
-SELECT TabelaVnes.GOD, [GODISNIK] & "1" AS sort, TabelaAdresar.GODISNIK_NAZIV AS NAZIV
-FROM TabelaVnes INNER JOIN TabelaAdresar ON (TabelaVnes.EVS = TabelaAdresar.EDINICASOS) AND (TabelaVnes.MATBR = TabelaAdresar.MATBR) AND (TabelaVnes.OTSEK = TabelaAdresar.SIFRAOTSEK)
-WHERE (((TabelaVnes.NACIN)="1"))
-GROUP BY TabelaVnes.GOD, [GODISNIK] & "1", TabelaAdresar.GODISNIK_NAZIV;
-
